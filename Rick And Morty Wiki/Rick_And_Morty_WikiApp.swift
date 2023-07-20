b214b99727f8015f1ad78e1baea13f460d30410d
Fran Alarza <franalarza@gmail.com>
2023-07-20 21:16:32 +0200

    New: app arch

---
+++ Rick And Morty Wiki/Rick_And_Morty_WikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_WikiApp.swift
+//  Rick and Morty Wiki
+//
+//  Created by Francisco Javier Alarza on 20/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_Morty_WikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
